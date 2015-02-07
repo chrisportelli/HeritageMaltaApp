28fd983f46d8018829058e3e308999bf0e6bfa77
Christopher Portelli <portellichristopher@gmail.com>
2015-02-07 22:31:43 +0100

    Profile page

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -3,3 +3,28 @@ $("#nav-toggle").click(function(e){
 	$("body").toggleClass("toggled");
 	$(this).toggleClass("active");
 });
+
+jQuery.fn.positionMe = function () {
+    var oH = $(window).height();
+    var oW = $(window).width();
+    var iH = this.outerHeight();
+    var iW = this.outerWidth();
+
+    // When the image is too small so, do nothing
+    if(iW>oW && iH>oH){
+
+        // Otherwise, proportionate the image relative 
+        // to its container
+        if(oH/iH > oW/iW){
+            this.css("width", oW);
+            this.css("height", iH*(oW/iW))
+        } else {
+            this.css("height", oH);
+            this.css("width", iW*(oH/iH));
+        }
+
+    }
+    return this;
+}
+
+$("#profile-image img").positionMe();
