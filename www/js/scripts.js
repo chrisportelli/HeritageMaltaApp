66a5854ce6b64205eda1912e36505bbaf0891d79
Christopher Portelli <portellichristopher@gmail.com>
2015-02-08 12:35:57 +0100

    Completion of profile page prototype

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -3,3 +3,30 @@ $("#nav-toggle").click(function(e){
     $("body").toggleClass("toggled");
     //$(this).toggleClass("active");
 });
+
+window.onload = function(){
+
+	var imageSrc = document
+					.getElementById('profile-image-landscape')
+					 .style
+					  .backgroundImage
+					   .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
+						.split(',')[0];
+
+	// I just broke it up on newlines for readability        
+
+	var image = new Image();
+	image.src = imageSrc;
+
+	var img_width = image.width,
+	img_height = image.height,
+	window_height = $(window).height(),
+	window_width = $(window).width();
+	if (img_width > window_width){
+		$("#profile-image-landscape").css("width", window_width);
+	}
+	else {
+		$("#profile-image-landscape").css("width", img_width).css("margin", "0 auto");
+	}
+	$("#profile-image-landscape").css("height", img_height).css("background-position", "50%");
+}
