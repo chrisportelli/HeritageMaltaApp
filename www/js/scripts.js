0a22024ee42591cf938265c6637f69442b426ef2
Christopher Portelli <portellichristopher@gmail.com>
2015-02-08 21:17:13 +0100

    Finalization of booking page and new history page

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,38 +5,33 @@ $("#nav-toggle").click(function(e){
 });
 
 $("ul#tickets-main-options li").click(function(){
-	var a = $(this).attr("rel");
-	$("#"+a).slideToggle();
-});
-
-$("li#opt1").click(function(){
-	$(".detailed-option").hide();
-	$(".chosen-option-calendar").animate({width:'toggle'},350);
+    var a = $(this).attr("rel");
+    $("#"+a).slideToggle();
 });
 
 window.onload = function(){
 
-	var imageSrc = document
-					.getElementById('profile-image-landscape')
-					 .style
-					  .backgroundImage
-					   .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
-						.split(',')[0];
+    var imageSrc = document
+                    .getElementById('profile-image-landscape')
+                     .style
+                      .backgroundImage
+                       .replace(/url\((['"])?(.*?)\1\)/gi, '$2')
+                        .split(',')[0];
 
-	// I just broke it up on newlines for readability        
+    // I just broke it up on newlines for readability        
 
-	var image = new Image();
-	image.src = imageSrc;
+    var image = new Image();
+    image.src = imageSrc;
 
-	var img_width = image.width,
-	img_height = image.height,
-	window_height = $(window).height(),
-	window_width = $(window).width();
-	if (img_width > window_width){
-		$("#profile-image-landscape").css("width", window_width);
-	}
-	else {
-		$("#profile-image-landscape").css("width", img_width).css("margin", "0 auto");
-	}
-	$("#profile-image-landscape").css("height", img_height).css("background-position", "50%");
+    var img_width = image.width,
+    img_height = image.height,
+    window_height = $(window).height(),
+    window_width = $(window).width();
+    if (img_width > window_width){
+        $("#profile-image-landscape").css("width", window_width);
+    }
+    else {
+        $("#profile-image-landscape").css("width", img_width).css("margin", "0 auto");
+    }
+    $("#profile-image-landscape").css("height", img_height).css("background-position", "50%");
 }
